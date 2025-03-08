5d0026f400285fffaca3670ecf38295f928bccaf
uddithmachiraju <uddithmachiraju@gmail.com>
2025-03-08 22:15:03 +0530

    Removed Errors

--- src/api/main.py
+++ src/api/main.py
@@ -1,5 +1,6 @@
 import json 
 import torch 
+from PIL import Image
 from src.models.model import CLIP 
 from flask import Flask, request, jsonify
 from src.models.text_encoder import tokenizer
@@ -43,52 +44,25 @@ mask = mask.repeat(1, len(mask[0])).reshape(len(mask), len(mask[0]), len(mask[0]
 
 # Define image preprocessing
 transform = transforms.Compose([
-    transforms.Resize((parameters['image_size'], parameters['image_size'])),
+    transforms.Resize((parameters['image_size'][0], parameters['image_size'][1])),
     transforms.ToTensor(),
-    transforms.Normalize(mean=[0.5], std=[0.5])  # Normalize for grayscale images
 ])
 
-app.route("/")
+@app.route("/")
 def home():
     return jsonify(
         {"message" : "CLIP model API is running! upload a image in '/predict' page to predictions"} 
-    )
+    ), 200
 
-app.route("/predict", methods = ['POST'])
+@app.route("/predict", methods=['POST'])
 def predict():
     if "image" not in request.files:
-        return jsonify(
-            {"Error" : "No Image uploaded"}
-        ), 400 
-    
-    image = request.files["image"] 
+        return jsonify({"Error": "No Image uploaded"}), 400
     try:
-        image = transform(image).unsqueeze(0).to(device)
-        with torch.no_grad():
-            image_features = model.image_encoder(image) 
-            text_features = model.text_encoder(text, mask = mask) 
-
-        # Normalize features 
-        image_features /= image_features.norm(dim = 1, keepdim = True)
-        text_features /= text_features.norm(dim = 1, keepdim = True) 
-
-        # Compute similarity scores 
-        similarity = (100 * image_features @ text_features.T).softmax(dim = -1)
-        values, indices = similarity[0].topk(5)
-
-        # Format predictions
-        predictions = [
-            {"Class" : class_names[int(index)], 
-             "Confidence": f"{100 * value.item():.2f}%"}
-             for value, index in zip(values, indices)
-        ]
-        return jsonify(
-            {"Predictions": predictions}
-        )
+        pass
     except Exception as e:
-        return jsonify(
-            {"Error": str(e)}
-        ), 500  
-    
+        print(f"Internal Server Error: {e}")
+        return jsonify({"Error": str(e)}), 500
+
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", port = 5000) 
