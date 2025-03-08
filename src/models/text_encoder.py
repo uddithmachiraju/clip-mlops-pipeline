fa2550f278345bd0c876cd7d68e9fd051e71d146
uddithmachiraju <uddithmachiraju@gmail.com>
2025-03-08 13:00:33 +0530

    updated imports and errors

--- src/models/text_encoder.py
+++ src/models/text_encoder.py
@@ -1,7 +1,7 @@
 import torch
 from torch import nn 
-from pos_embeds import PositionalEncoding
-from transformer_encoder import TransformerEncoder
+from src.models.pos_embeds import PositionalEncoding
+from src.models.transformer_encoder import TransformerEncoder
 
 def tokenizer(text, encode = True, mask = None, max_seq_length = 32):
     if encode:
@@ -16,7 +16,7 @@ def tokenizer(text, encode = True, mask = None, max_seq_length = 32):
         mask = torch.cat((mask, torch.zeros(max_seq_length - len(mask)))) 
     else:
         # Decode the text 
-        output = [chr[x] for x in text[1:len(mask.nonzero() - 1)]] 
+        output = [chr(x) for x in text[1:len(mask.nonzero() - 1)]] 
         output = "".join(output)
         mask = None 
 
@@ -42,7 +42,11 @@ class TextEncoder(nn.Module):
         x = self.positional_embeds(x)
         for encoder_layer in self.encoder:
             x = encoder_layer(x, mask) 
-        x = x[torch.arange(text.shape[0]), torch.sub(torch.sum(mask[:, 0], dim = 1), 1)] 
+        x = x[
+            torch.arange(text.shape[0], dtype=torch.int64),  # Ensure LongTensor
+            torch.sub(torch.sum(mask[:, 0], dim=1), 1).to(torch.int64)  # Convert to int64
+        ]
+ 
 
         if self.projection is not None:
             x = x @ self.projection 
