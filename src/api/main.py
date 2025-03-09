9e93fb12eb3cb366487a936b213d4820fc0f8aa1
uddithmachiraju <uddithmachiraju@gmail.com>
2025-03-09 09:40:08 +0530

    Updated Errors

--- src/api/main.py
+++ src/api/main.py
@@ -1,51 +1,6 @@
-import json 
-import torch 
-from src.models.model import CLIP 
 from flask import Flask, request, jsonify
-from src.models.text_encoder import tokenizer
-from torchvision.transforms import transforms 
-
-with open("src/data/parameters.json", "r") as file:
-    parameters = json.load(file) 
-
-# Class names for predictions
-class_names = [
-    "t-shirt/top", "trousers", "pullover", "dress", "coat",
-    "sandal", "shirt", "sneaker", "bag", "ankle boot"
-]
 
 app = Flask(__name__) 
-
-device = 'cpu'
-model = CLIP(
-    parameters['Emb_dim'],
-    parameters['vit_hidden_dim'],
-    parameters['image_size'],
-    parameters['patch_size'],
-    parameters['n_channels'],
-    parameters['vit_layers'],
-    parameters['vit_heads'],
-    parameters['vocab_size'],
-    parameters['text_dim'],
-    parameters['max_sequence'],
-    parameters['text_heads'],
-    parameters['text_layers']
-).to(device)
-
-# Load pretrained weights
-model.load_state_dict(torch.load("saved-models/CLIP.pt", map_location = device)) 
-model.eval() 
-
-# Prepare text encodings for the class names
-text = torch.stack([tokenizer(x)[0] for x in class_names]).to(device) 
-mask = torch.stack([tokenizer(x)[1] for x in class_names]) 
-mask = mask.repeat(1, len(mask[0])).reshape(len(mask), len(mask[0]), len(mask[0])).to(device) 
-
-# Define image preprocessing
-transform = transforms.Compose([
-    transforms.Resize((parameters['image_size'][0], parameters['image_size'][1])),
-    transforms.ToTensor(),
-])
 
 @app.route("/")
 def home():
